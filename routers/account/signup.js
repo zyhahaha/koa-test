b413eb7cdf71e73e37183e5d65672e655ce9caf1
zhaoyang <18355403288@163.com>
2018-12-20 22:43:24 +0800

    rename

--- routers/account/signup.js
+++ routers/account/signup.js
@@ -1,5 +1,5 @@
 const router = require('koa-router')();
-const controller = require('../../controller/account/c-signup')
+const controller = require('../../controller/account/signup_ctrl')
 
 // 注册页面
 router.get('/signup', controller.getSignup)
