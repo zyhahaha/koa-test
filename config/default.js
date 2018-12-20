dbe8f7d2fe57a6e4b380ddd3bda582f53c04b093
zhaoyang <18355403288@163.com>
2018-12-20 21:20:39 +0800

    update

---
+++ config/default.js
@@ -0,0 +1,14 @@
+const config = {
+  // 启动端口
+  port: 3000,
+  // 数据库配置
+  database: {
+    DATABASE: 'koa_test',
+    USERNAME: 'root',
+    PASSWORD: '854275944618',
+    PORT: '3306',
+    HOST: 'localhsot'
+  }
+}
+
+module.exports = config
