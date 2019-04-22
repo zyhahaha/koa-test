8fe5c1f17f5eaccb1520a4e03e0e3d4a53f45200
zhaoyang <18355403288@163.com>
2019-04-22 15:27:09 +0800

    update

--- config/default.js
+++ config/default.js
@@ -1,14 +1,15 @@
+const LOCAL_CONFIG = require('./local.js');
 const config = {
   // 启动端口
   port: 8088,
   // 数据库配置
   database: {
-    DATABASE: 'test',
-    USERNAME: 'root',
-    PASSWORD: '854275944618',
-    PORT: '3306',
-    HOST: '127.0.0.1'
+    DATABASE: LOCAL_CONFIG.database,
+    USERNAME: LOCAL_CONFIG.username,
+    PASSWORD: LOCAL_CONFIG.password,
+    PORT: LOCAL_CONFIG.port,
+    HOST: LOCAL_CONFIG.host
   }
-}
+};
 
-module.exports = config
+module.exports = config;
